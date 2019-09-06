b42cb09b8f0e4d59c79914424ee46bd52a2d9fd9
Joy <614106721@qq.com>
2019-09-06 16:32:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Qcode/>
+  </div>
+</template>
+
+<script>
+import Qcode from './components/Qcode'
+
+export default {
+  name: 'app',
+  components: {
+    Qcode
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Qcode.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="main">
+        <div>
+            <input type="text" v-model="text"><button @click="makeCode(text)">生成二维码</button>
+        </div>
+        <div style="width: 132px;height: 132px">
+            <div id="qrcode"></div> <!-- 创建一个div，并设置id为qrcode -->
+        </div>
+    </div>
+</template>
+
+<script>
+    import QRCode from 'qrcodejs2'  // 引入qrcode
+    export default {
+        name : 'Qcode',
+        data () {
+            return {
+                text: 'http://www.baidu.com',
+                codeObj: null
+            }
+        },
+        mounted () {
+            this.qrcode();
+        },
+        methods: {
+            qrcode() {
+                this.codeObj = new QRCode('qrcode', {
+                    width: 132,
+                    height: 132,
+                    colorDark : "#000",
+                    colorLight : "#fff",
+                })
+            },
+            makeCode (text) {
+                this.codeObj.makeCode(text)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+ .main{
+     display: flex;
+     flex-direction: column;
+ }
+</style>
